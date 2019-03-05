05e97c051bd3cbd609bd1e2cfe704aa3482cc048
mrsteveson <patrick.steveson727@gmail.com>
2019-03-05 17:06:50 -0600

    making progress about 8 outa 10 done so far, not really sure how to tell if the load event listener worked or not so not counting it as done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
 // Your code goes here---------------------------------------->
-
-
-
-// preventDefault(); Utilized on the anchor tags.
-// Example from guided-lecture.
-
-// const anchorTag = document.querySelectorAll('a');
-// anchorTag.addEventListener('click', function(event){
-//  console.log(`Anchor was clicked!`);
-//  // This will prevent the form from submitting to action_page.php
-//   event.preventDefault();
-// });
-
 // Event Listeners Section
 // Mouseover Event Listener
 // Changes ATag color to blue when moused-over.
@@ -22,6 +9,7 @@ for(let i = 0; i < aTag.length; i++){
         aTag[i].style.color = "blue";
     });
 }
+
 
 // Click Event Listener
 // Changes any Ptag section's text to purple and bolds them.
@@ -34,6 +22,7 @@ for(let i = 0; i < allPs.length; i++){
     });
 }
 
+
 // DblClick Event Listener
 // Image Nullifier!
 const imgNone = document.querySelectorAll('img');
@@ -44,6 +33,7 @@ for(let i = 0; i < imgNone.length; i++){
     });
 }
 
+
 // Wheel Event Listener 
 // When the mousewheel is scrolled. Sets background to pink.
 const wheelie = document.querySelector('.home');
@@ -52,45 +42,116 @@ wheelie.addEventListener('wheel', function(){
     wheelie.style.backgroundColor = "pink";
 });
 
+
 // KeyDown Event Listener
-// XXXXXXXXXXXXXXXXXXXXXXXXXX
+// Sets the background of the header section to yellow when a key is pressed.
 const keyDwn = document.querySelector('.main-navigation');
 
-keyDwn.addEventListener('keydown', function(){
-    keyDwn.style.backgroundColor = "yellow";
+document.addEventListener('keydown', function(){
+    keyDwn.style.backgroundColor= "yellow";
 });
 
-// Select Event Listener XXXXXXXXXXXXXXXXXXXXXXXXXXX
-// Changes selected text to be italic. Turns out not sure I can use select.
-const contentSelect = document.querySelectorAll('h2');
 
-for(let i = 0; i < contentSelect.length; i++){
-    contentSelect[i].addEventListener('select', function(){
-        contentSelect[i].style.fontStyle = "italic";
-    });
+// Drag Event Listener
+// Drag an anchor tag and it sets the background color to green.
+const draggable = document.querySelector('.nav-container');
+
+document.addEventListener('drag', function(e){
+    e.target.style.backgroundColor = 'green';
+});
+
+
+// ReSize Event Listener
+// Sets a border on container home
+const resized = document.querySelector('.home');
+
+document.addEventListener('resize', function(){
+    resized
+});
+
+
+// Scroll Event Listener
+// creates a div in the form of a red box, containing UpDown, as you scroll.
+let scrollY = 0; 
+scrollY = window.scrollY;
+    
+let div = document.createElement('div');
+document.body.append(div);
+div.id = "boxie";
+div.style.padding = "25px";
+div.style.color = "black";
+div.style.textAlign = "center";
+div.style.background = "red"
+div.innerHTML = "UpDown";
+div.style.borderRadius = '10px';
+div.style.position = "fixed";
+div.style.right = "100px";
+div.style.bottom = " 100px";
+
+
+
+window.addEventListener('scroll', function(e){
+    scrollY = window.scrollY;
+    if(scrollY > 100){
+        document.getElementById('boxie').style.display = "block";
+
+    } else if (scrollY < 100) {
+        document.getElementById('boxie').style.display = "none";
+    }
+});
+
+
+// Load Event Listener
+// Logs "Done Loading" when the lambdaschool about page finishes loading.
+// No idea how to gauge if this is working or not.
+const loading = document.createElement('script');
+
+loading.addEventListener('load', function(e){
+    console.log("Done loading");
+});
+
+loading.src = "https://lambdaschool.com/about/";
+loading.async = "true";
+
+
+// Focus Event Listener.
+// Changes background and color, when triggered.
+// Had to include the color in the global.less file to get it to work.
+function pause() {
+    document.body.classList.add('paused');
 }
 
+function play() {
+    document.body.classList.remove('paused');
+}
+
+window.addEventListener('blur', pause);
+window.addEventListener('focus', play);
 
 
 
+// Extra shizz------------------------------------------------------------->
 
+// preventDefault(); Utilized on the anchor tags.
+// Example from guided-lecture.
 
+// const anchorTag = document.querySelectorAll('a');
+// anchorTag.addEventListener('click', function(event){
+//  console.log(`Anchor was clicked!`);
+//  // This will prevent the form from submitting to action_page.php
+//   event.preventDefault();
+// });
 
+// ------------------------------------------------------------------------
+// Select Event Listener XXXXXXXXXXXXXXXXXXXXXXXXXXX
+// Changes selected text to be italic. Turns out not sure I can use select.
+// const contentSelect = document.querySelectorAll('h2');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// for(let i = 0; i < contentSelect.length; i++){
+//     contentSelect[i].addEventListener('select', function(){
+//         contentSelect[i].style.fontStyle = "italic";
+//     });
+// }
 
 
 // This one will turn every element I click on in the page to red.
